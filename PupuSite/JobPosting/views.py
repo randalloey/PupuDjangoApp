7bc8c0c3c0c4fb2e12e24630c9b290beacbceee2
Randall Oey <randalloey@gmail.com>
2012-12-24 23:04:45 -0800

    Chapter 3 djangobook

--- PupuSite/JobPosting/views.py
+++ PupuSite/JobPosting/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.http import HttpResponse
+import datetime
+
+def hello(request):
+    return HttpResponse("Hi Milky")
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html ="<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
